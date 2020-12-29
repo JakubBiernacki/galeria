7c1e3f5247db0f1dcdf27f765e9107545f380cc0
Jakub <mail>
2020-12-29 01:27:10 +0100

    wstępny profil, zdjecie profilowe

--- galeria/settings.py
+++ galeria/settings.py
@@ -136,7 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- galeria/urls.py
+++ galeria/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-#rejestracja
-from users import views as user_views
-#logowanie
 from django.contrib.auth import views as auth_views
+#static file
+from django.conf import settings
+from django.conf.urls.static import static
+
+from users import views as user_views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +32,6 @@ urlpatterns = [
     path("profile/", user_views.profile, name='profile'),
     path('',include('widok.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
