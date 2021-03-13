e21a0caeb3594dcb9fe3e05a97a7fff13d36385a
Jakub <mail>
2021-03-13 12:26:18 +0100

    łatanie luk w api

--- api/views.py
+++ api/views.py
@@ -5,20 +5,32 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
-# Create your views here.
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+# # Create your views here.
+#
 class KometarzeObrazkaViewSet(viewsets.ViewSet):
     serializer_class = KometarzSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
     def retrieve(self, request, pk=None):
+
         obrazek = get_object_or_404(Obrazek, id=pk)
         queryset = obrazek.kometarz_set.all().order_by('-data_publikacji')
         serializer = KometarzSerializer(queryset,many=True)
         return Response(serializer.data)
 
     def create(self, request):
+
         serializer = KometarzSerializer(data=request.data)
+
         if serializer.is_valid():
+
+            if request.user != serializer.validated_data['autor']:
+                return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
 
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -26,23 +38,28 @@ class KometarzeObrazkaViewSet(viewsets.ViewSet):
 
 class OcenyObrazkaViewSet(viewsets.ViewSet):
     serializer_class = OcenySerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
     def retrieve(self, request, pk=None):
         obrazek = get_object_or_404(Obrazek, id=pk)
         queryset = obrazek.oceny_set.all()
-        serializer = OcenySerializer(queryset,many=True)
-        return Response(serializer.data)
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
     def create(self, request):
 
-
-        serializer = OcenySerializer(data=request.data)
-
+        serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
+            autor = serializer.validated_data['autor']
+            if autor != request.user:
+                return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
+
             obrazek = serializer.validated_data['obrazek']
-            if not obrazek.czy_ocenil(serializer.validated_data['autor'].id):
+
+            if not obrazek.czy_ocenil(autor.id):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
 
